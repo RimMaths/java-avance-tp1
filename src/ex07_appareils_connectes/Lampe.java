ef4d4cf1a4c48a06acf8a3abcc8f33a6e07f1e5c
rim.el-guennouni.auditeur@lecnam.net <elguennounirim77@gmail.com>
2026-09-11 14:18:34 +0200

    exercice 7 - interface

--- src/ex07_appareils_connectes/Lampe.java
+++ src/ex07_appareils_connectes/Lampe.java
@@ -1,4 +1,20 @@
 package ex07_appareils_connectes;
 
-public class Lampe {
+public class Lampe implements Allumable {
+    private boolean allumee;
+    public Lampe() {
+        this.allumee = false;
+    }
+
+    @Override
+    public void allumer() {
+        allumee = true;
+        System.out.println("La lampe est allumée");
+    }
+
+    @Override
+    public void eteindre() {
+        allumee = false;
+        System.out.println("La lampe est éteinte");
+    }
 }
--- src/ex07_appareils_connectes/Main.java
+++ src/ex07_appareils_connectes/Main.java
@@ -1,4 +1,23 @@
 package ex07_appareils_connectes;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Allumable[] appareils = {
+                new TelephonePortable(),
+                new Lampe()
+
+        };
+
+        for (Allumable appareil : appareils) {
+            appareil.allumer();
+            if (appareil instanceof ConnectWifi) {
+
+                ConnectWifi appareilWifi =
+                        (ConnectWifi) appareil;
+
+                appareilWifi.connecter("WiFi_Cnam");
+            }
+        }
+    }
 }
