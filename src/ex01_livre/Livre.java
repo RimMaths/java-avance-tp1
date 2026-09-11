44297e8b7378d86245754c86ae609f0b731b6a9f
rim.el-guennouni.auditeur@lecnam.net <elguennounirim77@gmail.com>
2026-09-11 10:22:45 +0200

    Initial commit : Java advanced exercises

---
+++ src/ex01_livre/Livre.java
@@ -0,0 +1,15 @@
+package ex01_livre;
+
+public class Livre {
+
+    private String titre;
+    private String auteur;
+    private int nombrePages;
+    private boolean disponible;
+
+    // constructeur
+
+    // emprunter()
+
+    // rendre()
+}
---
+++ src/ex01_livre/Main.java
@@ -0,0 +1,12 @@
+package ex01_livre;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // création des livres
+
+        // tests emprunter / rendre
+
+    }
+}
