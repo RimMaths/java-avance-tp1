33ca93969be94a0e5116f8c5447190e632717dbc
rim.el-guennouni.auditeur@lecnam.net <elguennounirim77@gmail.com>
2026-09-11 10:45:56 +0200

    Complete exercise 1: Livre class

--- src/ex01_livre/Livre.java
+++ src/ex01_livre/Livre.java
@@ -8,8 +8,24 @@ public class Livre {
     private boolean disponible;
 
     // constructeur
-
+    public Livre(String titre, String auteur, int nombrePages) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.nombrePages = nombrePages;
+        this.disponible = true;
+    }
     // emprunter()
-
+    public void emprunter() {
+        if (disponible) {
+            disponible = false;
+            System.out.println("Le livre \"" + titre + "\" a été emprunté.");
+        } else {
+            System.out.println("Le livre \"" + titre + "\" n'est pas disponible.");
+        }
+    }
     // rendre()
+    public void rendre(){
+        disponible = true;
+        System.out.println("Le livre \"" + titre + "\" a été rendu.");
+    }
 }
--- src/ex01_livre/Main.java
+++ src/ex01_livre/Main.java
@@ -5,8 +5,31 @@ public class Main {
     public static void main(String[] args) {
 
         // création des livres
+        Livre livre1 = new Livre(
+                "1984",
+                "George Orwell",
+                328
+        );
+
+        Livre livre2 = new Livre(
+                "Le Petit Prince",
+                "Antoine de Saint-Exupéry",
+                96
+        );
 
         // tests emprunter / rendre
+        //tests sur livre 1 et 2
+        livre1.emprunter();
+        livre2.emprunter();
+        //Rendre le livre
+        livre1.rendre();
+        // Le livre peut maintenant être emprunté à nouveau
+        livre1.emprunter();
 
+        System.out.println();
+
+        // Test sur livre2
+        livre2.emprunter();
+        livre2.rendre();
     }
 }
